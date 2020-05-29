c789b4711fc8ecc0411838d41941489c07e0b5f5
Ryan Press <ryan@presslab.us>
2020-05-29 15:42:48 -0400

    presslab

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -361,10 +361,10 @@ const converters = {
         convert: (model, msg, publish, options, meta) => {
             if (msg.data.hasOwnProperty('lockState')) {
                 const state = getProperty('state', msg, model);
-                const lock_state = getProperty('state', msg, model);
+                const lock_state = getProperty('lock_state', msg, model);
                 const lookup = {0: 'not_fully_locked', 1: 'locked', 2: 'unlocked'};
                 return {
-                    [state]: msg.data.lockState == 1 ? 'LOCK' : 'UNLOCK'
+                    [state]: msg.data.lockState == 1 ? 'LOCK' : 'UNLOCK',
                     [lock_state]: lookup[msg.data['lockState']],
                 };
             }
